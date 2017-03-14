b6147e5d6223cef2550d56cd9a2b2bf578edbcde
Peter <peterallemann@hotmail.com>
2017-03-14 16:51:32 +0100

    Added interrupts to read from sensor

--- FirstSketch20170313/FirstSketch20170313.ino
+++ FirstSketch20170313/FirstSketch20170313.ino
@@ -1,3 +1,5 @@
+#include <TimerOne.h>
+
 
 
 /*
@@ -28,28 +30,58 @@
 
   
  */
-const int sensorPin = A7;    // select the input pin for the potentiometer
-const int buttonPin = 7;     // the number of the pushbutton pin
-const int ledPin =  2;        // the number of the LED pin
-const int current_val =185 ;     // datasheet says 185mV/A
+const int sensorPin = A7;       // select the input pin for the potentiometer
+const int buttonPin = 7;        // the number of the pushbutton pinf
+const int ledPin =  2;          // the number of the LED pin
+const int current_val =185 ;    // datasheet says 185mV/A
 const int offset_val  =2500;    // datasheet saya 0.5*Vcc Vcc=5V=2.5V
- int sensorValue=0;
+int sensorValue=0;
+volatile int sensorValues[500];
+int sensorValuesSize;
+volatile int sensorValuesIndx;
+const int readInterval = 2000;  // read sensor value every 2000 micro seconds (500 reads/s)
+
+int startTime;
+bool outputDone = false;
+
+void initializeSensorValues() {
+  sensorValuesSize = ( sizeof ( sensorValues ) / sizeof ( int ) );
+  for ( sensorValuesIndx = 0 ; sensorValuesSize > sensorValuesIndx ; sensorValuesIndx++ ) {
+    sensorValues [ sensorValuesIndx ] = 0;
+  }
+  sensorValuesIndx = 0;
+}
+
+void sensorRead() {
+  sensorValues [ sensorValuesIndx ] = analogRead ( sensorPin );
+  sensorValuesIndx = ( sensorValuesIndx + 1 ) % sensorValuesSize;
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-    Serial.begin(9600);
+  Serial.begin(9600);
   
+  initializeSensorValues();
+  Timer1.initialize ( readInterval );
+  Timer1.attachInterrupt ( sensorRead );
+  
+  startTime = micros();
+  delay(1000);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-   sensorValue = analogRead(sensorPin);
- Serial.print("sensor = ");
-  Serial.print(sensorValue);
-   Serial.println("");
-
-  delay(500);              // wait for a second
+  noInterrupts();
+  if ( !outputDone ) {
+  for ( sensorValuesIndx = 0 ; sensorValuesSize > sensorValuesIndx ; sensorValuesIndx++ ) {
+    Serial.print ( "sensor = " );
+    Serial.print ( sensorValues [ sensorValuesIndx ] );
+    Serial.println ( "" );
+  }  
+  outputDone = true;
+  }
+  interrupts();
 }
 
 
